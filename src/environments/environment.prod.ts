fce84cd7753eaef7aa299a3739bff27737348b01
月亮 <dommolino28@gmail.com>
2024-05-21 07:44:06 +0800

    url:

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl:
-    'https://srv1158-files.hstgr.io/ffe0b5cb1b00f261/files/public_html/backend/api/',
+  apiUrl: 'http://gc-attendease.online/backend/api/',
 };
